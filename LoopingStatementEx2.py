27431d9e0a537e6b2c407f6427dc30500737089c
krishsuresh08 <sureshkrishnan.r1652@gmail.com>
2022-09-12 16:03:54 +0530

    Python Loop Pattern

---
+++ LoopingStatementEx2.py
@@ -0,0 +1,10 @@
+#Pattern
+
+for i in range(1,6):
+    for j in range(1,i):
+        print(j,end='')
+    print()
+for x in range(1,6):
+    for y in range(1,7-x):
+        print(y,end='')
+    print()
